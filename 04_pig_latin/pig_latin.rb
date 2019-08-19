2c9d1631b984191e40e341e7b63fbfc10a852d57
snordale <snordale@gmail.com>
2019-08-19 10:37:56 -0400

    06 done

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,48 @@
 #write your code here
+
+def translate str
+	vowels = ['a', 'e', 'i', 'o', 'u']
+	sarray = str.split(' ')
+	sent = []
+	pre, root, first, second = ''
+
+	i = 0
+	sarray.each do |word|
+		first = word[i, 1]
+		second = word[i + 1, 1]
+		third = word[i + 2, 1]
+		
+		if vowels.include? first
+			pre = ''
+			root = word
+		else
+			if first == 'q'
+				pre = 'qu'
+				root = word[i + 2 .. -1]
+			elsif !vowels.include? second
+				if !vowels.include? third
+					if third == 'q'
+						pre = first + second + 'qu'
+						root = word[i + 2 .. -1]
+					else 
+						pre = first + second + third
+						root = word[i + 3 .. -1]
+					end
+				elsif second == 'q'
+					pre = first + 'qu'
+					root = word[i + 3 .. -1]
+				else
+					pre = first + second
+					root = word[i + 2 .. -1]
+				end
+			else
+				pre = first
+				root = word[i + 1 .. -1]
+			end
+		end
+		puts(root + ',,,  ' + pre + '  ' + 'ay')
+		sent.push(root + pre + 'ay')
+	end
+	sent = sent.join(' ')
+	sent
+end
--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,21 @@
 class Book
 # write your code here
+    attr_reader :title
+
+    def title=(t)
+        sarray = t.split(' ')
+        sol = []
+        conjunctions = ['and', 'or', 'but', 'so', 'for', 'nor', 'yet']
+        articles = ['the', 'a', 'an']
+        prepositions = ['in', 'with', 'before', 'behind', 'of', 'out', 'about', 'till', 'to', 'near']
+        sarray.each do |word|
+            if ((!conjunctions.include? word) && (!articles.include? word) && (!prepositions.include? word))
+                sol.push(word.capitalize)
+            else 
+                sol.push(word)
+            end
+        end
+        sol[0] = sol[0].capitalize
+        @title = sol.join(' ')
+    end 
 end
